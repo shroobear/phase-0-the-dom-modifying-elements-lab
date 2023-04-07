1022baf4581eacf2157722a241d76e3cf0f496d2
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-04-07 19:54:53 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// // Write your code here!
+// const main = document.getElementById("main");
+
+// main.remove();
+// const element = document.createElement("h1");
+// element.id = 'victory';
+// document.body.append(element);
+
+// let newHeader = document.getElementById("victory")
+
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = "Stan is the champion";
